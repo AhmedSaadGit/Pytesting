50c75ffadbd2326c58ab7914d4207e43b9ebeeaa
AhmedSaadGit <76616865+AhmedSaadGit@users.noreply.github.com>
2022-05-22 17:27:34 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
+import os
 import time
 from selenium import webdriver
 
+    #setting up the chrome driver 
+os.environ['PATH'] += r"./"
+driver = webdriver.Chrome()
 
+driver.get("https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal")
 
-driver = webdriver.Chrome(".venv\chromedriver.exe")
-driver.implicitly_wait(10)
-driver.maximize_window()
+#time.sleep(3)
+driver.implicitly_wait(30) #apply across all /waiting for the element to charge for 30 s , once charged click immediatly
+HomeElement = driver.find_element_by_id("n-thema")
+HomeElement.click()
 
-driver.get("https://google.com")
+TextSourceElement = driver.find_element_by_class_name("mw-list-item")
+print(f"{TextSourceElement} Hello world ")
 
+driver.close
 
 
